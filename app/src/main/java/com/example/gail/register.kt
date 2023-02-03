9271a69fd3a9ab57231b2212aababe453ce424b4
Parth Nanda <parthnanda002@gmail.com>
2023-02-03 15:37:20 +0530

    Added new navigation fragment

---
+++ app/src/main/java/com/example/gail/register.kt
@@ -0,0 +1,11 @@
+package com.example.gail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_register)
+    }
+}
---
+++ app/src/main/java/com/example/gail/ui/register/DashboardFragment.kt
@@ -0,0 +1,42 @@
+package com.example.gail.ui.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.gail.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val registerViewModel =
+            ViewModelProvider(this).get(RegisterViewModel::class.java)
+
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textRegister
+        registerViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
