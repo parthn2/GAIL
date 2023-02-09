710ada82f3dacbd66993798e1771464ed6ab146a
Parth Nanda <parthnanda002@gmail.com>
2023-02-09 15:34:59 +0530

    Added registration form

--- app/src/main/java/com/example/gail/ui/register/DashboardFragment.kt
+++ app/src/main/java/com/example/gail/ui/register/DashboardFragment.kt
@@ -7,36 +7,41 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import android.widget.Toast
 import com.example.gail.databinding.FragmentRegisterBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterFragment : Fragment() {
 
-    private var _binding: FragmentRegisterBinding? = null
+    private lateinit var binding : FragmentRegisterBinding
+    private lateinit var database : DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+        binding.button.setOnClickListener {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+            val firstName = binding.motorId.text.toString()
+            val lastName = binding.current.text.toString()
+            val age = binding.voltage.text.toString()
+            val userName = binding.power.text.toString()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val registerViewModel =
-            ViewModelProvider(this).get(RegisterViewModel::class.java)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            val Motor = Motor(firstName,lastName,age,userName)
+            database.child(userName).setValue(Motor).addOnSuccessListener {
 
-        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+                binding.motorId.text.clear()
+                binding.current.text.clear()
+                binding.voltage.text.clear()
+                binding.power.text.clear()
 
-        val textView: TextView = binding.textRegister
-        registerViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+                Toast.makeText(this,"Successfully Saved",Toast.LENGTH_SHORT).show()
+
+            }.addOnFailureListener{
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            }
         }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/gail/ui/register/Motor.kt
@@ -0,0 +1,6 @@
+package com.example.gail.ui.register
+
+data class Motor(val motorId : String? = null,val voltage : String? = null,val current : String? = null,val power : String? = null){
+
+
+}
